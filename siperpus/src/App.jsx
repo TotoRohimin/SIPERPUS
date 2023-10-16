973577191b127dbec1c685bc58f8a29fa9924853
toomiinn <butouchiha9@gmail.com>
2023-10-16 10:01:39 +0700

    Update App.jsx

--- siperpus/src/App.jsx
+++ siperpus/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1>
+        Example heading
+        <Badge bg="secondary" as={Button}>
+          New
+        </Badge>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
