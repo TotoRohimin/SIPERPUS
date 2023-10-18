9afaf31250b6351e8d6dba83c9765e236d881d89
toomiinn <butouchiha9@gmail.com>
2023-10-18 14:36:41 +0700

    Update App.jsx

--- siperpus/src/App.jsx
+++ siperpus/src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Badge from "react-bootstrap/Badge";
-import Button from "react-bootstrap/Button";
+import Beranda from "./component/Beranda/Beranda";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>
-        Example heading
-        <Badge bg="secondary" as={Button}>
-          New
-        </Badge>
-      </h1>
+      <Beranda />
     </div>
   );
 }
