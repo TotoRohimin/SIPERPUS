77846a8dd480b420920c47d0f2e2f1652cf3ac35
toomiinn <butouchiha9@gmail.com>
2023-10-20 14:57:38 +0700

    Create PeminjamanPage.jsx

---
+++ siperpus/src/pages/PeminjamanPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Peminjaman from "../component/Peminjaman/Peminjaman";
+
+function PeminjamanPage() {
+  return (
+    <div>
+      <Peminjaman />
+    </div>
+  );
+}
+
+export default PeminjamanPage;
