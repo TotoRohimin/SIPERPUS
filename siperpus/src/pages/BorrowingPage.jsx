0f1855b0ac777d15af8a229d27bac5683dc31b87
toomiinn <butouchiha9@gmail.com>
2023-10-23 06:59:13 +0700

    Create BorrowingPage.jsx

---
+++ siperpus/src/pages/BorrowingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Peminjaman from "../component/Peminjaman/Peminjaman";
+
+function BorrowingPage() {
+  return (
+    <div>
+      <Peminjaman />
+    </div>
+  );
+}
+
+export default BorrowingPage;
