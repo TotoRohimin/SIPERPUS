b4191b1c331e5a9c12377e2945e4faae331f4f19
toomiinn <butouchiha9@gmail.com>
2023-10-23 06:59:38 +0700

    Create ReportPage.jsx

---
+++ siperpus/src/pages/ReportPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Laporan from "../component/Laporan/Laporan";
+
+function ReportPage() {
+  return (
+    <div>
+      <Laporan />
+    </div>
+  );
+}
+
+export default ReportPage;
