e5cd53582d43eee41c20a6fd544a01be24ca0281
toomiinn <butouchiha9@gmail.com>
2023-10-23 22:30:22 +0700

    Create BookPage.jsx

---
+++ siperpus/src/pages/BookPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Book from "../component/Buku/Buku";
+import { Nav } from "react-bootstrap";
+
+function BookPage() {
+  return (
+    <div>
+      <Book />
+    </div>
+  );
+}
+
+export default BookPage;
