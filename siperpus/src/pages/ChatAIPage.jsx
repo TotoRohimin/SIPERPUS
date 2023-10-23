723e14e72e366438999b18371162359be342bcc1
toomiinn <butouchiha9@gmail.com>
2023-10-24 06:09:18 +0700

    Create ChatAIPage.jsx

---
+++ siperpus/src/pages/ChatAIPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatAI from "../component/ChatAI/Chat";
+
+function ChatAIPage() {
+  return (
+    <div>
+      <ChatAI />
+    </div>
+  );
+}
+
+export default ChatAIPage;
