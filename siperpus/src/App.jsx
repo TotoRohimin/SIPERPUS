86b409b0cefa4c4edbdad292ea79fd00316f31e6
toomiinn <butouchiha9@gmail.com>
2023-10-25 01:14:33 +0700

    Update App.jsx

--- siperpus/src/App.jsx
+++ siperpus/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import BookPage from "./pages/BookPage";
+import ChatAIPage from "./pages/ChatAIPage";
+import BorrowingPage from "./pages/BorrowingPage";
+import ReportPage from "./pages/ReportPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <BookPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/buku" element={<BookPage />} />
+        <Route path="/openai" element={<ChatAIPage />} />
+        <Route path="/peminjaman" element={<BorrowingPage />} />
+        <Route path="/laporan" element={<ReportPage />} />
+      </Routes>
+    </Router>
   );
 }
 
