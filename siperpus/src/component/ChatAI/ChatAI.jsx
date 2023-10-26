5d21b7bb9d5b0177ec4bc1f48add925840b4a26c
toomiinn <butouchiha9@gmail.com>
2023-10-26 11:36:02 +0700

    Create ChatAI.jsx

---
+++ siperpus/src/component/ChatAI/ChatAI.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import OpenAIApi, { OpenAI } from "openai";
+import { Container, Form, Button, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "./chat.css";
+
+function ChatAI() {
+  const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_APP_APIKEY_OPENAI,
+    dangerouslyAllowBrowser: true,
+  });
+
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt: prompt,
+        max_tokens: 100,
+      });
+      setResult(response.choices[0].text);
+      setData("");
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div id="navbar">
+      <Navbar expand="lg" className="navbar bg-light shadow fixed-top">
+        <Container>
+          <Navbar.Brand href="#beranda">
+            <b>SIPERPUS</b>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#beranda">Beranda</Nav.Link>
+              <Nav.Link href="#tentang">Tentang</Nav.Link>
+              <Nav.Link href="#kontak">Kontak</Nav.Link>
+              <Nav.Link href="#chat">Chat AI</Nav.Link>
+              <Nav.Link href="#buku">Buku</Nav.Link>
+              <NavDropdown title="Data Master" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Peminjaman</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Laporan</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <h1 className="display-6">Chat AI SIPERPUS</h1>
+        <div className="mx-auto">
+          <Form>
+            <Form.Group controlId="prompt">
+              <Form.Control type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} className="textarea" />
+            </Form.Group>
+
+            <div className="button-container">
+              <Button onClick={handleSubmit} disabled={loading || prompt.length === 0} variant="primary">
+                {loading ? "Generating..." : "Generate"}
+              </Button>
+            </div>
+          </Form>
+          <div className="response">
+            <p>{result}</p>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default ChatAI;
